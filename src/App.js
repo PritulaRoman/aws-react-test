f978f6ac7c39ed5b805ffd1962cde27b4787da5c
Roman Prytula <roman_prytula@epam.com>
2020-03-16 21:30:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ReactJS application for brutal men 1.
+          ReactJS application for brutal men 2.
         </p>
       </header>
     </div>
